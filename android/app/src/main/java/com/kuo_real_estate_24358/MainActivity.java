73156a555ae38046a50b8df239b960de9cb065b5
Crowdbotics <team@crowdbotics.com>
2021-02-07 18:19:26 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/kuo_real_estate_24358/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kuo_real_estate_24358;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "kuo_real_estate_24358";
+    }
+}
---
+++ ios/kuo_real_estate_24358Tests/kuo_real_estate_24358Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface kuo_real_estate_24358 : XCTestCase
+
+@end
+
+@implementation kuo_real_estate_24358
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
